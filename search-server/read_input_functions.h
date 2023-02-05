8e856b450c13dc4e9a489f90b6160d3b6814c956
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-02-06 00:01:35 +0400

    Create read_input_functions.h

---
+++ search-server/read_input_functions.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string ReadLine();
+
+int ReadLineWithNumber();
+
+std::vector<std::string> SplitIntoWords(const std::string&);

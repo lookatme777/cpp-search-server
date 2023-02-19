afb99691f9ebd479099043ce5e7f19b18b559c73
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-02-19 18:07:49 +0400

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -1,10 +1,5 @@
 #include "string_processing.h"
 
-
-std::ostream& operator << (std::ostream& out, const Document search) {
-    return out << "{ document_id = " << search.id << ", relevance = " << search.relevance << ", rating = " << search.rating << " }";
-}
-
 std::vector<std::string> SplitIntoWords(const std::string& text) {
     std::vector<std::string> words;
     std::string word;

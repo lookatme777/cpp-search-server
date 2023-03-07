d50b82709eb2e4814bb6493d8a6754142b9b1b3f
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-03-07 10:40:20 +0400

    Update remove_duplicates.cpp

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,34 +1,36 @@
 #include "remove_duplicates.h"
 
-void RemoveDuplicates(SearchServer& search_server) 
-{ 
-    
-  std::map<std::set<std::string>, int> dupl; 
-  std::set<std::string> unique; 
-  std::set<int> id_delete; 
- 
+void RemoveDuplicates(SearchServer& search_server)
+{
 
-  for (const int document_id : search_server)  
-  { 
-    std::map<std::string, double> array = search_server.GetWordFrequencies(document_id); 
- 
+    std::set<std::set<std::string>> dupl;
+    std::set<std::string> unique;
+    std::set<int> id_delete;
 
-    transform(begin(array), end(array), inserter(unique, unique.begin()), [](auto String) { return String.first; }); 
- 
 
-    if(!dupl.count(unique)) 
-    { 
-      dupl.emplace(unique, document_id); 
-    } 
-    else 
-    { 
-      id_delete.emplace(document_id); 
-    } 
-    unique.clear(); 
-  } 
-  for (auto& id : id_delete) 
-  { 
-    std::cout << "Found duplicate document id " << id << std::endl; 
-    search_server.RemoveDocument(id); 
-  } 
+    for (const int document_id : search_server)
+    {
+        std::map<std::string, double> array = search_server.GetWordFrequencies(document_id);
+
+
+        transform(begin(array), end(array), inserter(unique, unique.begin()), [](auto String) { return String.first; });
+
+
+        if (!dupl.count(unique))
+        {
+            dupl.emplace(unique);
+        }
+        else
+        {
+            id_delete.emplace(document_id);
+        }
+        unique.clear();
+    }
+    for (auto& id : id_delete)
+    {
+        std::cout << "Found duplicate document id " << id << std::endl;
+        search_server.RemoveDocument(id);
+    }
 }
+
+

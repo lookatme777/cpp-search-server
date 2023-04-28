0ab7d476f5e3ff758e0f53d5ca538508eea09cf7
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-04-28 14:38:02 +0400

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -1,22 +1,16 @@
 #include "string_processing.h"
 
-std::vector<std::string> SplitIntoWords(const std::string& text) {
-    std::vector<std::string> words;
-    std::string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        }
-        else {
-            word += c;
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
+std::vector<string_view> SplitIntoWords(string_view str)
+{
+    vector<string_view> result;
+    int64_t pos = str.find_first_not_of(" ");
+    const int64_t pos_end = str.npos;
 
-    return words;
+    while (pos != pos_end) {
+        int64_t space = str.find(' ', pos);
+        result.push_back(space == pos_end ? str.substr(pos) : str.substr(pos, space - pos));
+        pos = str.find_first_not_of(" ", space);
+    }
+    return result;
 }
+

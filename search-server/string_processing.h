2632d4d062065746b03e22a120957922ed8fa2dc
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-04-28 14:37:44 +0400

    Update string_processing.h

--- search-server/string_processing.h
+++ search-server/string_processing.h
@@ -7,14 +7,14 @@
 using namespace std;
 
 template <typename StringContainer>
-std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
-    std::set<std::string> non_empty_strings;
-    for (const std::string& str : strings) {
+std::set<std::string, std::less<>> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    std::set<std::string, std::less<>> non_empty_strings;
+    for (auto str : strings) {
         if (!str.empty()) {
-            non_empty_strings.insert(str);
+            non_empty_strings.emplace(str);
         }
     }
     return non_empty_strings;
 }
 
-std::vector<std::string> SplitIntoWords(const std::string&);
+std::vector<std::string_view> SplitIntoWords(std::string_view str);
